f52e67b68d7fde38805cc6c9021fac6920574989
Amir Omidi <amir@aaomidi.com>
2018-09-05 17:06:18 +0430

    Auto install apps.

---
+++ brew.sh
@@ -0,0 +1,46 @@
+echo "Installing xcode-stuff"
+xcode-select --install
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure everything is up to date
+brew update
+
+# Install git
+brew install git
+
+git config --global user.name "Amir Omidi"
+git config --global user.email "amir@aaomidi.com"
+
+tools=(
+  git
+  tree
+  wget
+  mtr  
+)
+
+
+# Apps
+apps=(
+  dropbox
+  discord
+  firefox
+  google-chrome
+  private-internet-access
+  steam
+  spotify
+  sublime-text
+  vlc
+  transmission
+  jetbrains-toolbox
+  visual-studio-code
+  java8
+  java
+)
+
+echo "Installing tools with homebrew"
+brew install ${tools[@]}
+
+echo "Installing apps with homebrew"
+brew cask install ${apps[@]}
